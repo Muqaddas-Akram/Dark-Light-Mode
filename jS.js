00d8dd1be5528cfc99f13318095e948bbfe511be
Muqaddas-Akram <muqaddasakram416@gmail.com>
2024-08-08 23:27:43 +0500

    first commit

---
+++ jS.js
@@ -0,0 +1,15 @@
+function circle_Button() {
+    var rectangle = document.querySelector('.rectangle');
+    rectangle.classList.toggle('move-right');
+    rectangle.classList.toggle('change-image');
+    var color = document.body
+    color.classList.toggle('change-color')
+    var circle = document.querySelector('.circle')
+    circle.classList.toggle('change-bgColor')
+}
+
+
+
+    
+
+
